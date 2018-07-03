50a9d8b1705ac4aa98661b85c69159079101ff06
Ali <atajadod94@gmail.com>
2018-07-02 21:20:10 -0400

    updated home navigation

--- js/script.js
+++ js/script.js
@@ -3,16 +3,21 @@
 var html_dictionary = {
   "Getintouch" : "snippets/getintouch.html",
   "Services" : "snippets/services.html",
-  "about" : "snippets/home.html",
+  "About" : "snippets/about.html",
+  "Renovation" : "snippets/home.html"
 };
 
 
 $(document).ready(function(){
   $('.sidenav').sidenav();  // materialize code
   $(".nav-item a").addClass("nav_button") // styling all nav_buttons
+  $(".brand-logo").click(function(){
+    var clicked = $(this).html().replace(/\s/g, '');
+    console.log(clicked)
+    $(".body").load(html_dictionary[clicked]) ;
+  });
   $(".nav-item a").click(function(){
-    var clicked = $(this).html().replace(/\s/g, '')
-    console.log(html_dictionary[clicked])
+    var clicked = $(this).html().replace(/\s/g, '');
     $(".body").load(html_dictionary[clicked]) ;//adding html_butto
-  })
+  });
 })
